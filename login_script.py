7f61ac7d836a8105c31e9d3b71523618f5a1a26c
Anand <58105052+anand-lyn@users.noreply.github.com>
2025-05-14 10:36:25 +0800

    Update login_script.py

--- login_script.py
+++ login_script.py
@@ -5,6 +5,9 @@ import requests
 def send_telegram_message(message):
     bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
     chat_id = os.environ.get('TELEGRAM_CHAT_ID')
+    if not bot_token:
+        print('not found bot config')
+        return
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     payload = {
         "chat_id": chat_id,
